bcbcaed7dbfe04a38066cd9ff3d7824c65d556ef
stumpy252 <123128220+stumpy252@users.noreply.github.com>
2023-01-21 14:21:22 -0500

    added forces to ball and started to get it to move

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,17 +6,25 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+    private float movementX;
+    private float movementY;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent <Rigidbody> ;
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
     void FixedUpdate()
     {
-        
-    }
-    void OnMove(InputValue movementValue)
-    {
-        Vector2 movementVector = movementValue.get<Vector2>;
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement);
     }
 }
