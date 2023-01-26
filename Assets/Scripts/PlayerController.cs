c205146eda9e892e0b4b95ab4300e2ddfc587ea8
stumpy252 <123128220+stumpy252@users.noreply.github.com>
2023-01-26 18:31:35 -0500

    created winning text and added script to trigger win text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
     private Rigidbody rb;
     private int count;
     private float movementX;
@@ -15,6 +19,9 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
+
+        SetCountText();
+        winTextObject.SetActive(false);
     }
     
     void OnMove(InputValue movementValue)
@@ -23,6 +30,15 @@ public class PlayerController : MonoBehaviour
 
         movementX = movementVector.x;
         movementY = movementVector.y;
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if(count >= 12)
+        {
+            winTextObject.SetActive(true);
+        }
     }
     void FixedUpdate()
     {
@@ -36,6 +52,8 @@ public class PlayerController : MonoBehaviour
         {
             other.gameObject.SetActive(false);
             count += 1;
+
+            SetCountText();
         }
     }
 }
